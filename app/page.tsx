2425be26ae0998d5a3a3114a69e3dab2c84547a8
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-02-23 15:51:11 +0300

    feat: fetch products from API and define Product type

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import getProducts from "../server/product";
 import {
   Card,
   CardContent,
@@ -7,23 +8,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const products = getProducts();
   return (
     <main className="flex flex-col justify-center gap-10 max-w-6xl mx-auto ">
       <h1>Awoseme Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        {(await products).map((product) => (
+          <Card key={product.id}>
+            <CardHeader>
+              <CardTitle>{product.title}</CardTitle>
+              <CardDescription>{product.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </main>
   );
---
+++ server/product.ts
@@ -0,0 +1,9 @@
+"use server";
+
+import { Product } from "@/components/shared/types";
+
+export default async function getProducts(): Promise<Product[]> {
+  const res = await fetch("https://api.escuelajs.co/api/v1/products");
+  const data = await res.json();
+  return data;
+}
